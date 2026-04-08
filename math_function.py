6a8ab7c4dacee3f7471f020b760c67f0ba8571d0
dadsena01 <manish.dadsena13@gmail.com>
2026-04-08 18:47:08 +0530

    feat: using math function

---
+++ math_function.py
@@ -0,0 +1,22 @@
+import math
+# result = math.sqrt(100)
+# print(result)
+
+# # sin
+# print(math.sin(0.00))
+# print(math.sin(-1.23))
+# print(math.sin(10))
+# print(math.sin(math.pi))
+# print(math.sin(math.pi / 2))
+
+# # cos
+# print(math.cos(0.00))
+# print(math.cos(-1.23))
+# print(math.cos(10))
+# print(math.cos(math.pi))
+# print(math.cos(math.pi / 2))   
+
+
+# Problem: add 1, sin(23.45), cos(23.45) and sqrt(23.45) and give the result
+result= 1 + math.sin(23.45) + math.cos(23.45) + math.sqrt(23.45)
+print(result)
