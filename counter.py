63e60832c76a372d0c21546cf96642e1e362ada0
dadsena01 <manish.dadsena13@gmail.com>
2026-04-09 10:44:46 +0530

    feat: using counter function

---
+++ counter.py
@@ -0,0 +1,10 @@
+from collections import Counter
+sentence = "hello world"
+letter_count = Counter(sentence)       
+print(letter_count)
+
+
+bag = ["red", "blue", "red", "yellow", "blue", "red"]
+counts = Counter(bag)
+print(counts)
+
